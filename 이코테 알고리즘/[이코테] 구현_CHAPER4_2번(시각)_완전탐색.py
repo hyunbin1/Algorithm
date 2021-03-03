96a11e3d362d05cee206d4a55fd11afeccc380b3
hyunbin1 <kimgusqls1@gmail.com>
2021-03-03 23:12:59 +0900

    chapter4 - 구현 2번_완전탐색 알고리즘

--- 이코테 알고리즘/[이코테] 구현_CHAPER4_2번(시각)_완전탐색.py
+++ 이코테 알고리즘/[이코테] 구현_CHAPER4_2번(시각)_완전탐색.py
@@ -1,3 +1,5 @@
+# 방법 1(내가 짠 코드)
+# 시간복잡도 예측: N*3? == N
 total = 0
 count = 0
 minute = 0
@@ -20,3 +22,16 @@ for t in range(n+1):
         total += minute
 
 print(total)
+
+# 방법 2(교재 서술)
+# 예상 시간복잡도: n**3
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for i in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(k) + str(j) + str(i):
+                count += 1
+print(count)
