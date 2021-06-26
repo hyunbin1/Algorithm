979d5e6bc944b4775ae6fa3af3786a340f251e99
hyunbin1 <kimgusqls1@gmail.com>
2021-06-26 12:15:04 +0900

    10872번_피보나치 수 5(재귀)

--- Java/Main.java
+++ Java/Main.java
@@ -1,26 +1,26 @@
 import java.util.*;
 /*  
-10872번_팩토리얼_재귀
-*/
-
-/*
-
+10872번_피보나치 수 5(재귀)
 */
 class Main {
     public static void main(String[] args)
 {
     Scanner scan = new Scanner(System.in);
     int input = scan.nextInt();
-    int result = 1;
-    if(input == 0){
-    System.out.println(1);
-}else{
-    for(int i =1; i <= input; i++){
-        result = result * i;
+    int a0 = 0;
+    int a1 = 1;
+    int result = 0;
+    if(input <= 1 ){
+        System.out.println(input);
+    }else{
+
+    for(int i = 0; i < input-1; i++){
+        result = a0 + a1;
+        a0 = a1;
+        a1 = result;
     }
     System.out.println(result);
-}
-
+    }
 }
 }
 
