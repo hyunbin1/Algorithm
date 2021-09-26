795d521b31383f38229b4e573073c2185c7fb982
hyunbin1 <kimgusqls1@gmail.com>
2021-09-26 23:55:37 +0900

    3주차 알고리즘 강의

--- Java/Number1181.java
+++ Java/Number1181.java
@@ -4,14 +4,15 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 /*
+* 60200216 김현빈
 * 1. 첫 번째 입력은 단어의 개수 = Num으로 입력 받기
 * 2. 각 단어 배열화 하기
 * 3. 글자수가 같은 단어끼리 서로 같은지 비교하고 하나 제거하기.
 * */
 
-
-
 public class Number1181 {
+
+  // 시작
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         // 입력받을 총 단어 개수 입력 받기
@@ -22,12 +23,13 @@ public class Number1181 {
             // 단어들 배열에 넣기
             words[i] = scanner.next();
         }
+        // 영어 순에 따라 단어 재배열
         Arrays.sort(words);
+        // 단어 길이에 따라 단어 오름차순으로 재배열
         Arrays.sort(words, Comparator.comparing(String::length));
 
-        String temp = "";
-
         //만일 같은 단어가 나올 시에 무시하기
+        String temp = "";
         for(int j = 0; j < num; j ++){
             if (!temp.equals(words[j])) {
                 temp = words[j];
@@ -35,5 +37,4 @@ public class Number1181 {
             }
         }
     }
-
 }
---
+++ Java/test.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+
+public class test {
+
+    private static void quickSort(int[] arr){
+        quickSort(arr, 0, arr.length-1);
+    }
+    private static void quickSort(int[] arr, int startIndex, int endIndex){
+        int rightPartStartIndex = partition(arr, startIndex, endIndex);
+        if(startIndex < rightPartStartIndex-1){
+            quickSort(arr, startIndex, rightPartStartIndex -1);
+        }
+        if(rightPartStartIndex<endIndex){
+            quickSort(arr, rightPartStartIndex, endIndex);
+        }
+    }
+
+    private static int partition(int[] arr, int startIndex, int endIndex){
+        int pivot = arr[(startIndex+endIndex)/2];
+        while(startIndex <= endIndex){
+            while (arr[startIndex] < pivot) {
+                startIndex++;
+            }
+            while (arr[endIndex] > pivot){
+                endIndex--;
+            }
+            if(startIndex <= endIndex){
+                swapNumberArray(arr, startIndex, endIndex);
+                swapWordsArray();
+                startIndex++;
+                endIndex--;
+            }
+        }
+        return startIndex;
+    }
+
+    private static void swapNumberArray(int[] arr, int startIndex, int endIndex){
+        int temp = arr[startIndex];
+        arr[startIndex] = arr[endIndex];
+        arr[endIndex] = temp;
+    }
+
+    private static void swapWordsArray(String[] arr, int startIndex, int endIndex){
+        String temp = arr[startIndex];
+        arr[startIndex] = arr[endIndex];
+        arr[endIndex] = temp;
+    }
+
+
+
+    public static void main(String[] args){
+        String[] words = {
+                "but", "i", "wont", "hesitate", "no", "more",
+                "no", "more", "it", "cannot", "wait", "im", "yours"
+        };
+        int num = words.length;
+
+        int[] wordLength = new int[num];
+        for(int i=0; i< words.length; i++){
+            wordLength[i] = words[i].length();
+        }
+
+        int right = 0, left = num;
+        int pivot = num/2;
+
+        while(right <= left){
+            while(wordLength[right]<pivot){
+                right++;
+            }
+            while(wordLength[left]>pivot){
+                left--;
+            }
+            if(right <= left){
+                String temp = words[right];
+                words[right] = words[left];
+                words[left] = temp;
+                left--;
+            }
+        }
+
+
+
+
+        // 배열 출력
+        System.out.println(Arrays.toString(words));
+        System.out.println(Arrays.toString(wordLength));
+
+
+    }
+}
