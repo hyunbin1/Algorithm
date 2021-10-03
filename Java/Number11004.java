0e46ee2d054f650a5b41ac1b4174f26b2a06f99b
hyunbin1 <kimgusqls1@gmail.com>
2021-10-03 13:38:29 +0900

    21/09/19 11004 백준 알고리즘 퀵정렬 직접 사용하기

--- Java/Number11004.java
+++ Java/Number11004.java
@@ -2,79 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Number11004 {
-
-    public static void swap(int[] arr, int num1, int num2) {
-        int temp = arr[num1];
-        arr[num1] = arr[num2];
-        arr[num2] = temp;
-    }
-
-    public static int partition(int[] arr, int start, int right){
-        int pivot = arr[start]; // 시작점을 피벗위치로 고정
-        int left = start + 1;
-
-        while(left <= right){
-            while(pivot >= arr[left] && left <= right){
-                left++;
-            }
-
-            while(pivot <= arr[right] && right >= (start +1) ){
-                right--;
-            }
-
-            if(left <= right ){
-                swap(arr, left, right);
-            }
-        }
-        // 왼쪽과 오른쪽 인텍스가 교차될 경우 피벗을 교차된 것으로 변경해주기
-        swap(arr, start, right);
-        return right;
-    }
-
-    public static int quickSearch(int[] arr, int left, int right, int KIndex){
-
-        int pivot = partition(arr, left, right);
-        if(pivot == KIndex){
-            return arr[pivot];
-        }
-
-        // 왼쪽 정렬
-        else if(pivot > KIndex){
-            quickSearch(arr, left, pivot-1, KIndex);
-        }
-        //오른쪽 정렬
-        else{
-            quickSearch(arr, pivot + 1, right, KIndex);
-        }
-        return arr[KIndex];
-    }
-
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int number = Integer.parseInt(st.nextToken());
-        int KIndex = Integer.parseInt(st.nextToken()) - 1;
-        int arr[] = new int[number];
-
-//        ArrayList<Integer> list = new ArrayList<>();
+        int KIndex = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < number; i++) {
-            arr[i] = (Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-//        Collections.sort(list);
-//        System.out.println(list.get(KIndex-1));
-        System.out.println(quickSearch(arr, 0 , number-1, KIndex));
+        Collections.sort(list);
+
+        System.out.println(list.get(KIndex-1));
 
         br.close();
         bw.close();
     }
-
-
-
-
 }
