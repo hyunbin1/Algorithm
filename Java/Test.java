69678d6750b3798cf512aed9ae1f97fe5477caaf
hyunbin1 <kimgusqls1@gmail.com>
2021-10-19 22:24:03 +0900

    [test] 상위 클래스에서 기본 생성자가 없으면 오류가 남

---
+++ Java/Test.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+class B{
+
+//    public B(){
+//        System.out.println("B");
+//    }
+//
+    public B(int b){
+        System.out.println("B");
+    }
+
+}
+
+public class Test extends B{
+
+    public void Text() {
+        System.out.println("test");
+    }
+
+}
