ac4f1386be786a9b4efcc6384ee23ca323a5036b
hyunbin1 <kimgusqls1@gmail.com>
2021-12-19 17:13:37 +0900

    [add] 기본 입출력 - 사칙연산

---
+++ Java/Baekjoon/기본입출력/Number10869.java
@@ -0,0 +1,18 @@
+package Baekjoon.기본입출력;
+import java.util.Scanner;
+
+public class Number10869 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
