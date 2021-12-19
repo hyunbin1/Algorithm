dacf7711e9be45d8957e7446d0cdcfb610ed5234
hyunbin1 <kimgusqls1@gmail.com>
2021-12-19 17:09:26 +0900

    [add] 기본 입출력 - Hello World!

--- Java/Baekjoon/Greedy/Number1931.java
+++ Java/Baekjoon/Greedy/Number1931.java
@@ -36,6 +36,7 @@ class Meeting implements Comparable<Meeting>{
     int start;
     int end;
 
+
     public Meeting(int start, int end){
         this.start = start;
         this.end = end;
---
+++ Java/Baekjoon/기본입출력/Number2557.java
@@ -0,0 +1,7 @@
+package Baekjoon.기본입출력;
+
+public class Number2557 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
